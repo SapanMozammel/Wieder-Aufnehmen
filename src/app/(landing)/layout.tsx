eb82f7ad4e510ed7c8075eb51b37f88e986ea53d
Sapan Mozammel <sapanmozammel@gmail.com>
2024-04-29 10:46:47 +0600

    ThemeProvider

--- src/app/(landing)/layout.tsx
+++ src/app/(landing)/layout.tsx
@@ -1,12 +1,20 @@
 import Footer from '@/components/landing/Footer';
 import Header from '@/components/landing/Header';
+import { ThemeProvider } from 'next-themes';
 
 const layout = ({ children }: { children: React.ReactNode }) => {
 	return (
 		<>
-			<Header />
-			<main>{children}</main>
-			<Footer />
+			<ThemeProvider
+				enableSystem={true}
+				defaultTheme="system"
+				enableColorScheme={false}
+				themes={['light', 'dark', 'system']}
+				attribute="class">
+				<Header />
+				<main>{children}</main>
+				<Footer />
+			</ThemeProvider>
 		</>
 	);
 };
