9cf08bacefa826a366f996aa862f74d2d72384bd
Sapan Mozammel <sapanmozammel@gmail.com>
2024-04-29 10:48:54 +0600

    Update index.tsx

--- src/components/landing/Header/index.tsx
+++ src/components/landing/Header/index.tsx
@@ -1,5 +1,15 @@
+'use client'
+
+import { useTheme } from 'next-themes';
 const Header = () => {
-	return <div>Header</div>;
+	const { theme, setTheme } = useTheme();
+	return (
+		<div className='space-x-4'>
+			<button onClick={() => setTheme('light')}>Light</button>
+			<button onClick={() => setTheme('dark')}>Dark</button>
+			<button onClick={() => setTheme('system')}>system</button>
+		</div>
+	);
 };
 
 export default Header;
