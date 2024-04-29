d7444f85fb79c1b1be84678e16f3e26d1dd743e9
Sapan Mozammel <sapanmozammel@gmail.com>
2024-04-29 11:11:40 +0600

    Update index.tsx

--- src/components/landing/Hero/index.tsx
+++ src/components/landing/Hero/index.tsx
@@ -1,5 +1,5 @@
 const Hero = () => {
-	return <section className="min-h-screen bg-white dark:bg-black">
+	return <section className="min-h-screen bg-white dark:bg-black text-dark dark:text-white">
 		<div className="container">
 			Lorem, ipsum dolor sit amet consectetur adipisicing elit. Labore, commodi?
 		</div>
