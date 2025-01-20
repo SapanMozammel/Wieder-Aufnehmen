b5be14de5b125f17404b85f56a38f6e7af554e1b
Sapan Mozammel <sapanmozammel@gmail.com>
2025-01-20 17:15:21 +0600

    WIP

--- src/components/landing/Hero/AdminScreen.tsx
+++ src/components/landing/Hero/AdminScreen.tsx
@@ -1,7 +1,7 @@
 const AdminScreen = () => {
 	return (
-		<div className="w-full aspect-[16/10] bg-primary-600/20">
-			AdminScreen
+		<div className="w-full aspect-[16/10] relative text-base">
+			<div className="h-full w-full flex flex-col bg-white/20 dark:bg-dark/20 backdrop-blur-sm rounded-[0.5em] border-[0.025em] border-solid border-info/30 shadow-lg shadow-info/10"></div>
 		</div>
 	);
 };
--- src/components/landing/Hero/index.tsx
+++ src/components/landing/Hero/index.tsx
@@ -6,11 +6,11 @@ import HeroBackground from './HeroBackground';
 
 const Hero = () => {
 	return (
-		<section className="min-h-screen flex flex-col pt-20">
+		<section className="flex flex-col pt-20">
 			<div className="grow flex flex-col relative z-1">
 				<HeroBackground>
 					<SectionSeparator lts rts lbs rbs bl ll rl>
-						<div className="container grow py-20 flex flex-col items-center justify-center gap-4 w-full text-center">
+						<div className="container grow pt-16 sm:pt-24 lg:pt-32 flex flex-col items-center justify-center gap-4 w-full text-center">
 							<h1 className="sr-only">
 								Hi, I am Sapan Mozammel, <br /> a fullstack
 								frontend developer.
@@ -36,7 +36,7 @@ const Hero = () => {
 								fill>
 								Let&apos;s Talk
 							</LinkButton>
-							<div className="mt-10 w-3/4">
+							<div className="mt-8 sm:mt-10 lg:mt-14 w-3/4 -mb-[20vw]">
 								<AdminScreen />
 							</div>
 						</div>
