a845aaef3cd0b8594f2059cbd8a9a4adedd2ead2
Sapan Mozammel <sapanmozammel@gmail.com>
2025-02-08 23:47:44 +0600

    Update ThemeSwitcher.tsx

--- src/components/landing/Header/ThemeSwitcher.tsx
+++ src/components/landing/Header/ThemeSwitcher.tsx
@@ -78,9 +78,13 @@ const ThemeSwitcher = () => {
 		<DropdownMenu>
 			<TooltipProvider>
 				<Tooltip>
-					<TooltipTrigger className="outline-none h-8 aspect-square inline-flex items-center justify-center cursor-pointer text-secondary-500 hover:text-primary dark:text-secondary-300 dark:hover:text-success ease-in-out">
+					<TooltipTrigger asChild>
 						<DropdownMenuTrigger asChild>
-							<span>{themeIcon(theme)}</span>
+							<button
+								type="button"
+								className="outline-none h-8 aspect-square inline-flex items-center justify-center cursor-pointer text-secondary-500 hover:text-primary dark:text-secondary-300 dark:hover:text-success ease-in-out">
+								{themeIcon(theme)}
+							</button>
 						</DropdownMenuTrigger>
 					</TooltipTrigger>
 					<TooltipContent
